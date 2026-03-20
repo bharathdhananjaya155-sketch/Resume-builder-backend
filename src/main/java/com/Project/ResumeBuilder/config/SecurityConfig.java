45e45a812df5feaa21be1ed8d428fceffc5b5263
Bharath D <james@Bharaths-MacBook-Air.local>
2026-03-20 08:16:05 +0530

    Add security config

--- src/main/java/com/Project/ResumeBuilder/config/SecurityConfig.java
+++ src/main/java/com/Project/ResumeBuilder/config/SecurityConfig.java
@@ -6,7 +6,9 @@ import com.Project.ResumeBuilder.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -87,5 +89,10 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source. registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
